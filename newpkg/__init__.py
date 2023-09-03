a1ffa506eb8e9aac515638806739a3175b058bcf
Mike Knerr <53237403+mkner@users.noreply.github.com>
2023-09-03 17:38:44 -0400

    Update __init__.py

--- newpkg/__init__.py
+++ newpkg/__init__.py
@@ -1,14 +1,9 @@
-"""
-new-pkg
-"""
 
-# flit uses __version__ as one source version 
+# newpkg
 
-__version__ = 'v0.01.01'
+__version__ = 'v0.01.01c'
 
 # import acts to export 
 
 from .code import Package
-
-
 
