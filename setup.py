d02c58bad02bfc01efdc4892e9a720730268a8d6
Mike Knerr <53237403+mkner@users.noreply.github.com>
2023-09-03 17:43:18 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,27 @@
+#! /usr/bin/env python
 
-DISTNAME = "new-pkg"
-DESCRIPTION = "new-pkg test project for setuptools"
+from setuptools import Command, Extension, setup
+from setuptools.command.build_ext import build_ext
 
-VERSION = robo_clocks.__version__  # gets from init??
 
-# import a restricted version of roboclocks
-# does not need the compiled code??
+DISTNAME = "new-pkgsetup"
+DESCRIPTION = "test project using setuptools"
 
+if __name__ == "__main__":
+    setup()
+ 
 #mk what this to work after pip install
-import new_pkg as newpkg
 
+import newpkg
+
+#import newpkg as somethingelse
+
+
+# example below is from scikit setup.py
+
+# We can actually import a restricted version of sklearn that
+# does not need the compiled code
+#import sklearn  # noqa
+#import sklearn._min_dependencies as min_deps  # noqa
+#from sklearn._build_utils import _check_cython_version  # noqa
+#from sklearn.externals._packaging.version import parse as parse_version  # noqa
