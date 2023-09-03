f685b2a7fadae364f1b4ed76c7b0ef28952b8b5c
Mike Knerr <53237403+mkner@users.noreply.github.com>
2023-09-03 14:51:47 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,12 @@
 
+DISTNAME = "new-pkg"
+DESCRIPTION = "new-pkg test project for setuptools"
+
+VERSION = robo_clocks.__version__  # gets from init??
+
+# import a restricted version of roboclocks
+# does not need the compiled code??
+
+#mk what this to work after pip install
+import new_pkg as newpkg
+
